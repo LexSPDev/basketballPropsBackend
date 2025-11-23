293cc157acd158fec471ed4db957baeb71bef927
lex <alejandro.roqve@gmail.com>
2025-11-23 02:08:37 -0600

    restore restore

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import gameRoutes from './route/game.route.js';
-import playerRoutes from './route/player.route.js';
-import { connectDB } from './config/db.js';
+import gameRoutes from '../route/game.route.js';
+import playerRoutes from '../route/player.route.js';
+import { connectDB } from '../config/db.js';
 
 
 dotenv.config();
